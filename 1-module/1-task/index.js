9e51e9dadd8ea89d4f2ed6966ea1dcb5bf7a0ab7
Kirill Malskiy <annete85@yandex.ru>
2021-11-11 11:59:56 +0300

    Написать функцию вычисления факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,19 @@
 function factorial(n) {
-  // ваш код...
+    if (!n) {
+        return 1;
+    } else {
+        let x = 1;
+
+        for(let i = 2; i <= n; i++) {    
+            x = x * i;
+        }
+
+        return x;
+    }
 }
+
+
+// alert ( factorial(0) );
+// alert ( factorial(1) );
+// alert ( factorial(3) );
+// alert ( factorial(5) );
