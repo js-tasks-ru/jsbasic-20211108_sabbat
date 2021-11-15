6cd3166fa19380da708c7e40c37db675602c84e7
Kirill Malskiy <annete85@yandex.ru>
2021-11-15 18:34:57 +0300

    Решить задачи второго занятия

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,31 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let allSalaries = [];
+
+  for (let key in salaries) {
+
+      if (typeof salaries[key] === 'number') 
+      {
+          allSalaries.push(salaries[key]);
+      }
+  }
+  
+  let sum = 0;
+  for (let salary of allSalaries){
+      if (salary !== "" && salary !== null && isFinite(salary)) {
+          sum += salary;
+      }
+  }
+  return sum;
 }
+
+let salaries = {
+  John: NaN,
+  Ann: 1000,
+  Pete: 0,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+
+alert( sumSalary(salaries) );
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
+    read() {
+      this.a = arguments[0];
+      this.b = arguments[1];
+    },
+
+    sum() {
+      return this.a + this.b;
+    },
+
+    mul() {
+      return this.a * this.b;
+    }
 };
+
+calculator.read(3, 5);
+alert( calculator.sum() );
+alert( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
